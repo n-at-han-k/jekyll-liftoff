13d708a6604890eaf5a73f7f01dafbd7211e6332
Nathan Kidd <nathanblenheimkidd@gmail.com>
2024-04-14 19:30:55 +0100

    Update importmap.rb

--- importmap.rb
+++ importmap.rb
@@ -6,4 +6,4 @@ pin_all_from "assets/js/controllers", under: "controllers", to: 'assets/js/contr
 pin "swiper",        to: "https://ga.jspm.io/npm:swiper@11.1.0/swiper.mjs"
 pin "swiper/bundle", to: "https://ga.jspm.io/npm:swiper@11.1.0/swiper-bundle.mjs"
 pin "@stimulus-components/carousel", to: "https://ga.jspm.io/npm:@stimulus-components/carousel@6.0.0/dist/stimulus-carousel.mjs"
-pin "@googlemaps/js-api-loader", to: "https://ga.jspm.io/npm:@googlemaps/js-api-loader@1.16.6/dist/index.mjs"`
+pin "@googlemaps/js-api-loader", to: "https://ga.jspm.io/npm:@googlemaps/js-api-loader@1.16.6/dist/index.mjs"
